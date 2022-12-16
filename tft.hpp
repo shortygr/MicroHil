1b6db3f415af54d5db3f29b824f66024462edd0c
shortygr <steffen38@gmx.net>
2022-12-16 19:48:56 +0100

    Revert "essage=Baseline rpm and wheel speed"
    This reverts commit 575e91bc3627e3f734f6c8663c61ba0e29a01419.

--- tft.hpp
+++
@@ -1,7 +0,0 @@
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-
-#define SPEED_MODE 0
-#define RPM_MODE   1
