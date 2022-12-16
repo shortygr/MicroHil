575e91bc3627e3f734f6c8663c61ba0e29a01419
shortygr <steffen38@gmx.net>
2022-12-16 19:46:16 +0100

    essage=Baseline rpm and wheel speed

---
+++ microhil.hpp
@@ -0,0 +1,19 @@
+int incSpeed = 10;
+int incRPM = 1000;
+int maxSpeed = 300;
+int maxRPM = 16000;
+float rollingCircumference = 1.959;
+int pulsesPerRotation = 20;
+
+int crankValue[120] = {LOW,LOW,LOW,LOW,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,
+                       LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH,LOW,HIGH};
